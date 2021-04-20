13bec45e65d2827e6981b3516f913d9ccf2cd1bd
Fierclash <fireboxpig@gmail.com>
2021-04-19 17:03:03 -0700

    Initial Commit

---
+++ Asset Shuffle 4/Assets/Scripts/Host.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 
+/// </summary>
+public class Host : MonoBehaviour
+{
+    #region Variables
+    public GameObject emote;
+    #endregion
+
+    /// <summary>
+    /// Allows an actor to emote
+    /// </summary>
+    /// <param name="emote"></param>
+    public void Emote(GameObject emote)
+    {
+        // Disable Old Emote
+        this.emote.SetActive(false);
+        // Replace with new Emote
+        this.emote = emote;
+        emote.SetActive(true);
+    }
+}
